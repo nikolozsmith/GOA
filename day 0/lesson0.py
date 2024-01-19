ce59d6145d42299b970c3e7c7e94c60cac78a531
nikolozsmith <nikadoodley@gmail.com>
2024-01-19 21:27:38 +0400

    first commit

---
+++ day 0/lesson0.py
@@ -0,0 +1,62 @@
+from turtle import *
+
+
+#we want to paint a house 
+
+#step one draw a sqare
+
+shape("turtle")
+
+speed(5)
+color("purple")
+
+width(5)
+
+forward(200)
+
+left(90)
+forward(200)
+
+left(90)
+forward(200)
+
+left(90)
+forward(200)
+#end of squaer 
+
+#drawing a door
+
+left(90)
+
+begin_fill()
+
+forward(70)
+color("yellow")
+left(90)
+forward(120)
+right(90)
+forward(60)
+right(90)
+forward(120)
+
+end_fill()
+
+penup()
+goto(200,200)
+pendown()
+
+color("red")
+
+begin_fill()
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+exitonclick()
+
+#we want to make windows 
+
+penup()
+
+goto(200,200,200,200,)
---
+++ day 1/lesson1.py
@@ -0,0 +1,32 @@
+#name = "nika"
+#name aris cvladi 
+# = aris cvladistvis mnishvnelobis mimniwebeli simbolo 
+# "nika" aris cvladistvis mnishvneloba
+print(name)
+
+surname = "smith"
+
+print(surname)
+#print funqcias gadaecema ekranze gamosatani obiecti 
+
+name = "nika" #es aris str (string) tipis cvladi 
+age = 22 # es aris int (integer) mteli ricxvi 
+height = 184.5 #es aris float tipis cvladi (atwiladi) 
+#boolean (bool) tipis cvladi 
+
+knows_programming = True #True an False 
+is_ugly = False 
+
+isugly = false   #snackecase(ubralod weris stili standartaulad)
+
+isUgly = False   # javaskriptuli camelcase 
+
+print(name + " " + surname) 
+
+print(type(age))
+print(type(name))
+print(type(surnmae))
+print(type(height))
+print(type(knows_programming))
+
+print(name + " " + str(age))
