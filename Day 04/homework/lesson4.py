74e159a5f71b02b6a7440b95ffb99ca522765108
nikolozsmith <nikadoodley@gmail.com>
2024-03-02 00:26:09 +0400

    commit'

---
+++ Day 04/homework/lesson4.py
@@ -0,0 +1,13 @@
+
+
+#i started writing the code for the sum of two numbers 
+number1 = input("please enter first number: ")
+number2 = input("please enter second number: ")
+print(int(number1) + int(number2))
+#i completed writing the code for the sum of two numbers 
+
+#lets print user name,age,how many years later do him want to time travel
+name = input("please enter your name: ")
+age = input("please enter your age: ")
+time_travel = input("please enter how many years do you want to time travel: ")
+print("your age in the future: " + str(int(age) + int(time_travel)))
