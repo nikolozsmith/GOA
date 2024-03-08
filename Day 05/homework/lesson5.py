f25e261310fd5ef0144b8b9127262b827394b687
nikolozsmith <nikadoodley@gmail.com>
2024-03-08 23:36:47 +0400

    commit

---
+++ Day 05/homework/lesson5.py
@@ -0,0 +1,11 @@
+ # შემოატანინეთ მომხმარებელს ორი მნიშნელობა input() ფუნქციის მეშვეობით, შეინახეთ ისინი ცვლადებში ხოლო შემდეგ შეამოწმეთ რა ტიპის მნიშვნელობები ინახება მოცემულ ცვლადებში type ფუნქციის გამოყენებით
+
+user_input1 = int(input("please enter number: "))
+user_input2 = input("please enter word: ")
+
+print(type(user_input1))
+print(type(user_input2))
+
+
+
+
