d603bd957a756a6259f636ad3eca19d3b6377262
nikolozsmith <nikadoodley@gmail.com>
2024-03-13 22:06:44 +0400

    commit

---
+++ Day 06/homework/lesson6.py
@@ -0,0 +1,68 @@
+
+
+user_input = int(input("please enter a number: "))
+user_input2 = float(user_input)
+print(user_input == user_input2)
+
+
+print(5>3)
+print(19>12)
+print(17>11)
+print(9>8)
+print(22>20)
+
+print(5>=5)
+print(8>=7)
+print(12>=9)
+print(13>=13)
+print(22>=19)
+
+print(5<7)
+print(12<15)
+print(2<5)
+print(4<10)
+print(7<14)
+
+print(6<=9)
+print(7<=7)
+print(5<=13)
+print(9<=9)
+print(16<=22)
+
+print(4==4)
+print(13==13)
+print(9==9)
+print(7==7)
+print(22==22)
+
+print(3!=5)
+print(7!=9)
+print(8!=10)
+print(13!=16)
+print(18!=22)
+
+num1 = int(input("please enter a number: "))
+num2 = int(input("please enter a second number: "))
+
+print(num1>num2)
+print(num1>=num2)
+print(num1<num2)
+print(num1<=num2)
+print(num1==num2)
+print(num1!=num2)
+
+num1 = int(input("please enter a number: "))
+num2 = num1 + 10
+
+print(num1>num2)
+print(num1>=num2)
+print(num1<num2)
+print(num1<=num2)
+print(num1==num2)
+print(num1!=num2)
+
+num1 = int(input("please enter a number: "))
+num2 = float(input("please enter a second number: "))
+
+print(num1==num2)
+print(num1!=num2)
